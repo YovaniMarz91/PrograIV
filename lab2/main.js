0aacf30a7d39b6efe51d906d3a68a3de34c826e1
Yovani Ulises Chinchilla Martinez <69612480+YovaniMarz91@users.noreply.github.com>
2021-02-13 17:41:05 -0600

    progreso2

--- lab2/main.js
+++ lab2/main.js
@@ -1,5 +1,7 @@
 var miUniversiad = openDatabase('universidad','1.0','Aplicacion de alumnos',5*1024*1024);
-window.id = 4;
+window.id = 0;
+window.base = 'http://localhost/PrograIV/lab2/';
+
 if(!miUniversiad){
     alert("Elnavegador no soporta Web SQL");
 }
@@ -18,11 +20,16 @@ var appVue = new Vue({
     methods:{
         guardarAlumno(){
             if (this.alumno.contrasena == this.alumno.contrasena2) {
+                this.obtenerAlumnos();
+
                 miUniversiad.transaction(tran=>{
-                    tran.executeSql('INSERT INTO alumnos(id_alumno,nombre,correo,direccion,telefono,contrasena) VALUES(?,?,?,?,?,?) ',
-                        [++id,this.alumno.nombre,this.alumno.correo,this.alumno.direccion,this.alumno.telefono,this.alumno.contrasena]);
+                    tran.executeSql('INSERT INTO alumnos(id_alumno,nombre,correo,direccion,telefono,contrasena,matricula) VALUES(?,?,?,?,?,?,?) ',
+                        [++id,this.alumno.nombre,this.alumno.correo,this.alumno.direccion,this.alumno.telefono,this.alumno.contrasena,0]);
                     //this.obtenerProductos();
                     //this.limpiar();
+
+                    alert('Registrado!');
+                    window.location.replace(base + "login.html");
                 }, err=>{
                     console.log( err );
                 });
@@ -32,54 +39,50 @@ var appVue = new Vue({
             }
             
         },
-        obtenerImg(e){
-            //IMG 1
-            let rutaTemp = URL.createObjectURL(e.target.files[0]);
-            this.producto.img = rutaTemp;
-            //IMG2
-            /*rutaTemp = URL.createObjectURL(e.target.files[1]);
-            this.producto.img2 = rutaTemp;*/
-        },
-        obtenerProductos(){
-            /*miUniversiad.transaction(tran=>{
-                tran.executeSql('SELECT * FROM productos',[],(index,data)=>{
-                    this.productos = data.rows;
+        obtenerAlumnos(){
+            miUniversiad.transaction(tran=>{
+                tran.executeSql('SELECT * FROM alumnos',[],(index,data)=>{
                     id=data.rows.length;
                 });
             }, err=>{
                 console.log( err );
-            });*/
+            });
         },
-        mostrarProducto(pro){
-            this.producto = pro;
-        },
-        limpiar(){
-            this.producto.codigo='';
-            this.producto.descripcion='';
-            this.producto.precio='';
-            this.producto.img='';
-        },
-        borrarProducto(pro){
-            /**
-             * BD Web SQL
-            */
-            if(confirm("¿Eliminar?")){
-                miUniversiad.transaction(tran=>{
-                    tran.executeSql('DELETE FROM productos WHERE idProducto=?', [pro.idProducto]);
-                    this.obtenerProductos();
-                    this.limpiar();
-                }, err=>{
-                    console.log( err );
+        login(){
+            miUniversiad.transaction(tran=>{
+                tran.executeSql('SELECT id_alumno FROM alumnos WHERE correo=? AND contrasena=?',[this.alumno.correo,this.alumno.contrasena],(index,data)=>{
+                    if (data.rows.length == 1) {
+                        $cookies.set('login', '1');
+                        window.location.replace(base + "registro.html");
+                    }
+                    else {
+                        alert('Datos incorrectos');
+                    }
                 });
-            }
+            }, err=>{
+                console.log( err );
+            });
         }
     },
     created(){
         miUniversiad.transaction(tran=>{
-            tran.executeSql('CREATE TABLE IF NOT EXISTS `alumnos` (`id_alumno` int(11) NOT NULL, `nombre` varchar(50), `correo` varchar(50), `direccion` text, `telefono` varchar(15), `contrasena` varchar(150), PRIMARY KEY (`id_alumno`));');
+            tran.executeSql('CREATE TABLE IF NOT EXISTS `alumnos` (`id_alumno` int(11) NOT NULL, `nombre` varchar(50), `correo` varchar(50), `direccion` text, `telefono` varchar(15), `contrasena` varchar(150), `matricula` int(1), PRIMARY KEY (`id_alumno`)); ');
         }, err=>{
             console.log( err );
         });
-        this.obtenerProductos();
+
+        /*miUniversiad.transaction(tran=>{
+            tran.executeSql('CREATE TABLE IF NOT EXISTS `materias` (`id_materia` int(11) NOT NULL, `nombre` varchar(50) DEFAULT NULL, PRIMARY KEY (`id_materia`));');
+        }, err=>{
+            console.log( err );
+        });
+
+        miUniversiad.transaction(tran=>{
+            tran.executeSql('CREATE TABLE IF NOT EXISTS `inscripcion` (`id_inscripcion` int(11) NOT NULL, `id_alumno` int(11) DEFAULT NULL, `id_materia` int(11) DEFAULT NULL, PRIMARY KEY (`id_inscripcion`), KEY `id_alumno` (`id_alumno`), KEY `id_materia` (`id_materia`), CONSTRAINT `FK_inscripcion_alumnos` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT `FK_inscripcion_materias` FOREIGN KEY (`id_materia`) REFERENCES `materias` (`id_materia`) ON DELETE CASCADE ON UPDATE CASCADE));');
+        }, err=>{
+            console.log( err );
+        });*/
+
+        
     }
 });
